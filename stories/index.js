9b05fe1799c5532e75e48a0dd6db7fc642d3200a
Milan P. Antonovic <milan.antonovic@gmail.com>
2018-03-21 17:33:20 +0100

    dev

---
+++ stories/index.js
@@ -0,0 +1,140 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { withKnobs, text, boolean, number, select } from '@storybook/addon-knobs/react';
+
+import 'react-day-picker/lib/style.css';
+import 'ol/ol.css';
+import 'semantic-ui-css/semantic.css';
+
+import {
+    store,
+    setting
+} from 'istsos3-core';
+
+import Mappa from '../lib/mappa/mappa';
+import FoisMap from '../lib/mappa/foisMap';
+
+import {
+    Uoms,
+    ObservedProperties,
+    ObservationTypes,
+    SamplingTypes,
+    Fois,
+    SensorForm,
+    FoiForm,
+    Humans
+} from '../lib';
+
+storiesOf('List', module)
+    .addDecorator(withKnobs)
+    .addDecorator(story => <Provider
+        store={store}>
+        <div style={{
+            padding: "2em"
+        }}>
+            {story()}
+        </div>
+    </Provider>)
+    .add('Uoms', () => (
+        <Uoms
+            layout={text('Label', 'list')}/>
+    ))
+    .add('Observed Properties', () => (
+        <ObservedProperties
+            layout={text('Label', 'list')}/>
+    ))
+    .add('Observation types', () => (
+        <ObservationTypes
+            layout={text('Label', 'list')}/>
+    ))
+    .add('Feature of interests', () => (
+        <Fois
+            layout={text('Label', 'list')}/>
+    ));
+
+storiesOf('Dropdown', module)
+    .addDecorator(withKnobs)
+    .addDecorator(story => <Provider
+        store={store}>
+        <div style={{
+            padding: "2em"
+        }}>
+            {story()}
+        </div>
+    </Provider>)
+    .add('Humans', () => (
+        <Humans
+            layout={text('Label', 'dropdown')}
+            onSelected={action('onSelected')}/>
+    ))
+    .add('Observed Properties', () => (
+        <ObservedProperties
+            layout={select('Layout', {
+                dropdown: 'dropdown',
+                mdropdown: 'mdropdown'
+            }, 'dropdown')}
+            onSelected={action('onSelected')}/>
+    ))
+    .add('Sampling Types', () => (
+        <SamplingTypes
+            layout={select('Layout', {
+                dropdown: 'dropdown'
+            }, 'dropdown')}
+            onSelected={action('onSelected')}/>
+    ));
+
+const foiType = {};
+foiType[setting._SAMPLING_POINT] = setting._POINT;
+// foiType[setting._SAMPLING_CURVE] = setting._CURVE;
+// foiType[setting._SAMPLING_SOLID] = setting._SOLID;
+// foiType[setting._SAMPLING_SURFACE] = setting._SURFACE;
+
+storiesOf('Forms', module)
+    .addDecorator(withKnobs)
+    .addDecorator(story => <Provider
+        store={store}>
+        <div style={{
+            padding: "0px 2em"
+        }}>
+            {story()}
+        </div>
+    </Provider>)
+    .add('Sensor', () => (
+        <SensorForm
+            layout={
+                select('Layout', [
+                    'metadata',
+                    'observedproperties'
+                ], 'metadata')
+            }
+            single={boolean("Single obs. prop.", false, 'frm-02-01')}
+            onChange={action('sensor-onChange')}/>))
+    .add('Feature of interest', () => (
+        <FoiForm
+            onChange={action('foi-onChange')}
+            foiType={
+                select('foiType', [
+                    null,
+                    setting._SAMPLING_POINT,
+                    setting._SAMPLING_CURVE,
+                    setting._SAMPLING_SOLID,
+                    setting._SAMPLING_SURFACE
+                ], null, 'frm-02-02')
+            }/>
+        ));
+
+storiesOf('Maps', module)
+    .addDecorator(withKnobs)
+    .addDecorator(story => <Provider
+        store={store}>
+        <div style={{
+            padding: "0px 2em"
+        }}>
+            {story()}
+        </div>
+    </Provider>)
+    .add('Simple', () => (<Mappa/>))
+    .add('With fois', () => (<FoisMap/>));
